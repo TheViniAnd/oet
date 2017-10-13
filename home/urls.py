6a2fb6d383fcb61c016e132efce41a99950e9f70
viniand59@gmail.com <viniand59@gmail.com>
2017-10-13 22:41:32 +0500

    pozirit

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,13 @@
 from django.conf.urls import url
+from django.views.generic import DetailView
+from django.conf import settings
+from .models import Post
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.home_list, name='home_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- home/views.py
+++ home/views.py
@@ -1,4 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.utils import timezone
+
 
 def home_list(request):
-    return render(request, 'home/home_list.html',{})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'home/home_list.html',{'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'home/post_detail.html',{'post': post})
